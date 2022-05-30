4272a216a2e917823dd0dd9636e13511242544f5
Jwilss3 <jessicalaurenwilson1@gmail.com>
2022-05-30 15:30:58 +0100

    Remove divider and format

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,7 +3,6 @@ import { formatRelative } from 'date-fns';
 import Avatar from '@mui/material/Avatar';
 import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import List from '@mui/material/List';
 
 const Message = ({
@@ -13,22 +12,25 @@ const Message = ({
     createdAt = null,
 }) => {
     return (
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-            {photoURL ? (
-                <Avatar src={photoURL} alt='avatar' width={50} height={50} sx={{ alignItems: "flex-start" }} />
-            ) : null}
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }} >
+            {
+                photoURL ? (
+                    <Avatar src={photoURL} alt='avatar' width={50} height={50} sx={{ alignItems: "flex-start" }} />
+                ) : null}
             {displayName ? <ListItemText primary={displayName}></ListItemText> : null}
-            {createdAt?.seconds ? (
-                <ListItemText secondary={formatRelative(new Date(createdAt.seconds * 1000), new Date())}>
-                </ListItemText>
-            ) : null}
+            {
+                createdAt?.seconds ? (
+                    <ListItemText secondary={formatRelative(new Date(createdAt.seconds * 1000), new Date())}>
+                    </ListItemText>
+                ) : null
+            }
             <React.Fragment>
                 <Typography sx={{ display: 'inline' }}
                     component="span"
                     variant="body2"
                     color="text.primary">{text}</Typography>
             </React.Fragment>
-        </List>
+        </List >
     );
 };
 
